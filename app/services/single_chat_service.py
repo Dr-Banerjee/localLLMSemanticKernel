acd7f93123e6b0dbecba969244d2cfa493c116c8
Dr-Banerjee <raieanmathphd@gmail.com>
2026-08-29 16:37:22 +0200

    Ticket8: Rename service

--- app/services/single_chat_service.py
+++ app/services/single_chat_service.py
@@ -22,7 +22,8 @@ def loadPrompt(fileName : str)-> str:
 #returns a semantic kernel function taking into account system as well as answer prompts.
 def createAnswerFunction()-> KernelFunctionFromPrompt:
     systemPrompt = loadPrompt("system_prompts.txt")
-    answerPrompt = loadPrompt("answer_prompts.txt")    
+    answerPrompt = loadPrompt("answer_prompts.txt")
+    #form a prompt  with sytem and answer prompts to be used in the kernel function    
     prompt = f"""
                 <message role="system">
                 {systemPrompt}
@@ -32,12 +33,15 @@ def createAnswerFunction()-> KernelFunctionFromPrompt:
                 {answerPrompt}
                 </message>
             """
+    #settings for prompt execution which says how varied the output should be, how many of the top probable tokens to consider,
+    #how many tokens to predict, and how to choose between multiple functions if applicable.
     settings = OllamaChatPromptExecutionSettings(
         temperature=0.7,
         top_p=0.8,
         num_predict=500,
         function_choice_behavior=FunctionChoiceBehavior.Auto(),
         )
+    #the kernel function that is suppossed to be expose the prompt to the LLM 
     answerFunction = KernelFunctionFromPrompt(
         function_name="generate_answer",
         description="Explains an English idiom to a child",
@@ -50,9 +54,11 @@ async def generate_response(userRequest: UserRequest) -> ResponseToUserRequest:
     kernel = createKernel()
     userInput = userRequest.userInput
     answerFunction = createAnswerFunction()
+    #the arguments replace the placeholders in the prompt with the actual user input. 
     arguments = KernelArguments(
         user_input=userInput,
-        )    
+        )
+    #the kernel invokes the kernel function with the arguments replacing the placeholders and returns the response from the LLM.    
     response = await kernel.invoke(answerFunction, arguments=arguments)
     responseToUserRequest = parseResponseText(response.value[0].items[0].text)
     return responseToUserRequest
