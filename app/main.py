e140c1adf10da6d878697b44dc1e9919bc9ee331
Dr-Banerjee <raieanmathphd@gmail.com>
2026-08-30 15:33:52 +0200

    Ticket2: turn the answer_service to a class

--- app/main.py
+++ app/main.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI
 from models.request import UserRequest
 from models.response import ResponseToUserRequest
-import services.answer_service as answerService
+from services.answer_service import AnswerService
 import logging 
 import sys
+
 # Configure application logging 
 logging.basicConfig( level=logging.INFO, stream=sys.stdout, format="%(asctime)s | %(levelname)s | %(name)s | %(message)s", ) 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+answerService = AnswerService()
 
 @app.get("/") 
 async def root(): 
     logger.info("Root endpoint called") 
     return {"message": "Hello World"}
 
-@app.post("/answer")
 #post endpoint to receive user input and return the response from the LLM for a single query
+@app.post("/answer")
 async def get_answer(request: UserRequest):
-    response = await answerService.process(request)    
+    response = await answerService.processSingleRequest(request)    
     return response
 
 #post endpoint to carryout conversations with the LLM
--- app/services/answer_service.py
+++ app/services/answer_service.py
@@ -3,15 +3,17 @@ from models.response import ResponseToUserRequest
 from services.single_chat_service import SingleChatService
 from services.chat_service import ChatService
 
-chatService = ChatService()
-singleChatService = SingleChatService()
+class AnswerService:
+    def __init__(self):
+        self.chatService = ChatService()
+        self.singleChatService = SingleChatService()
 
-#function to feed the user input to the singleChatService.
-async def process(request: UserRequest) -> ResponseToUserRequest:
-    #Feed the user input to the LLM service and return the generated response
-    return await singleChatService.generate_response(request)
+    #function to feed the user input to the singleChatService.
+    async def processSingleRequest(self, request: UserRequest) -> ResponseToUserRequest:
+        #Feed the user input to the LLM service and return the generated response
+        return await self.singleChatService.generate_response(request)
 
-#function to feed conversationId and user request to the chatService
-async def chatProcess(conversationId: int, request: UserRequest) -> str:    
-    return await chatService.processUserRequest(conversationId,request)
+    #function to feed conversationId and user request to the chatService
+    async def chatProcess(self, conversationId: int, request: UserRequest) -> str:    
+        return await self.chatService.processUserRequest(conversationId,request)
     
