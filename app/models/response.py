541425f618afd823d22ddcee1c54d315b7289195
Dr-Banerjee <raieanmathphd@gmail.com>
2026-08-30 14:09:11 +0200

    Ticket2: Write proper explanatory comments

--- app/models/response.py
+++ app/models/response.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 
 class ResponseToUserRequest(BaseModel):
-    #The string generated as response to the user input.
+    #The meaning of the idiom
     meaning: str
+    #The reason why the idiom means so
     reason: str
+    #an example of the idiom being used
     example: str
+    #small sentence that will help the kid remember the idiom
+    #and it's meaning
     remember: str
