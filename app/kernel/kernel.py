2cfd14232fee877857b79d3847b2e6095192a757
Dr-Banerjee <raieanmathphd@gmail.com>
2026-09-22 17:48:20 +0200

    Dockerise the whole application

--- app/kernel/kernel.py
+++ app/kernel/kernel.py
@@ -5,7 +5,7 @@ from plugins.idiom_plugin import IdiomPlugin
 def createKernel()-> Kernel:
     kernel = Kernel()
     ollamaService = OllamaChatCompletion(ai_model_id="gpt-oss:20b-cloud",
-                                          host="http://localhost:11434",
+                                          host="http://ollama:11434",
                                           )   # Replace with your desired model
     kernel.add_service(ollamaService)
     kernel.add_plugin(
